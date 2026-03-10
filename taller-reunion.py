833132a4251f2eeed8c661b630f9c8ec243ba883
rut-alt <rut.18.gonzalez@gmail.com>
2026-03-10 08:55:50 +0100

    Update taller-reunion.py

--- taller-reunion.py
+++ taller-reunion.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Taller Scoring - Europea Seguros",
-    page_icon="logo.png",  # tu logo
+    page_icon="REDUCCIO╠üN-AES-01.png",  # tu logo
     layout="wide"
 )
 
